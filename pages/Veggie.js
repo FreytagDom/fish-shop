a51160d6bfd43216469ebe34a18fd42a9c52bf73
FreytagDom <dominicfreytag@web.de>
2022-09-06 16:48:56 +0200

    add veggieImg

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import FischImg from "../public/pizza.jpg";
+
 export default function veggies() {
   return (
     <>
       <h1>Vegetarisch</h1>
+      <Image src={FischImg} alt="" />
+
       <p>Obst</p>
     </>
   );
