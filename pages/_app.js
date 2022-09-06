1c7b7c816fabe3217270d413d84bf5f5c74b7979
FreytagDom <dominicfreytag@web.de>
2022-09-06 21:25:16 +0200

    addLink

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import GlobalStyle from "../components/GlobalStyle";
-import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <GlobalStyle />
-      <Link />
+
       <Component {...pageProps} />
     </Layout>
   );
