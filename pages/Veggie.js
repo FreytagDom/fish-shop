5ffd496ab4ff186bd69a23d2559a8aeebffe0eaf
FreytagDom <dominicfreytag@web.de>
2022-09-06 21:10:27 +0200

    addLink

--- pages/Veggie.js
+++ pages/Veggie.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import FischImg from "../public/pizza.jpg";
+import PizzaVeggie from "../public/pizza.jpg";
 
 export default function veggies() {
   return (
     <>
       <h1>Vegetarisch</h1>
-      <Image src={FischImg} alt="" />
+      <Image src={PizzaVeggie} alt="" />
 
       <p>Obst</p>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Image from "next/image";
+import PizzaVeggie from "../public/pizza.jpg";
+
 export default function Home() {
   return (
     <>
       <h1>Recipes App</h1>
+      <Image src={PizzaVeggie} alt="" />
     </>
   );
 }
